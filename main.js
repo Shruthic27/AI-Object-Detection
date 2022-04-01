9c2985f477ea4cf9cf96b8184b696bf09af4c13e
Shruthic27 <76080986+Shruthic27@users.noreply.github.com>
2022-03-31 20:14:45 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+video="";
+status="";
+objects=[];
+
+function preload(){
+}
+
+function setup(){
+    canvas=createCanvas(380, 380);
+    canvas.position(480,100);
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380);
+    if (status!="") {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {    
+        document.getElementById("status").innerHTML="Status : Object Detected";
+
+    fill("#ff640a");
+    percent=floor(objects[i].confidence*100);
+    text(objects[i].label+" "+percent+"%", objects[i].x+15, objects[i].y+15);
+    noFill();  
+    stroke("#ff640a");
+    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+    }
+}
+
+function gotResult(error, results){
+    if (error) {
+      console.log(error);  
+    }
+    console.log(results);
+    objects=results;
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+    object_name=document.getElementById("object_name").value;
+}
+
+function modelLoaded(){
+    console.log("model Loaded!");
+    status=true;
+}
